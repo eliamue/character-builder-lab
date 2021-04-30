1ecdc3859906d3b3d0e8733863b3724e0b7ec90d
Elise Muellerleile <eliamue@live.com>
2021-04-29 21:43:17 -0500

    character alignment scale

--- app.js
+++ app.js
@@ -1,5 +1,87 @@
 // import functions and grab DOM elements
-
+import {
+    getLawfulImage,
+    getNeutralImage,
+    getChaoticImage,    
+} from './utils.js';
 // initialize state
 
 // set event listeners to update state and DOM
+
+//change name
+const nameInput = document.getElementById('name');
+const nButton = document.getElementById('nameButton');
+const nameDisplay = document.getElementById('ndisplay');
+nButton.addEventListener('click', () => {
+    nameDisplay.textContent = nameInput.value;
+});
+
+//change gender
+const genderInput = document.getElementById('gender');
+const genderButton = document.getElementById('genderButton');
+const genderDisplay = document.getElementById('gdisplay');
+genderButton.addEventListener('click', () => {
+    genderDisplay.textContent = genderInput.value;
+});
+
+//change race
+const raceInput = document.getElementById('race');
+const raceButton = document.getElementById('raceButton');
+const raceDisplay = document.getElementById('rdisplay');
+raceButton.addEventListener('click', () => {
+    raceDisplay.textContent = raceInput.value;
+});
+
+//change class
+const classInput = document.getElementById('class');
+const classButton = document.getElementById('classButton');
+const classDisplay = document.getElementById('cdisplay');
+classButton.addEventListener('click', () => {
+    classDisplay.textContent = classInput.value;
+});
+
+// lawfulgood pic display button
+//function showPicture() {
+ //   var img = document.getElementById('lgpic');
+  //  img.src = './assets/lawfulgood.jpg';
+   // img.style.display = 'block';
+//}
+//const lgButton = document.getElementById('lawful-good-button');
+//lgButton.addEventListener('click', () => {
+ //   showPicture();
+//});
+
+// lawful
+const lawfulInput = document.getElementById ('lawful-input');
+const lawButton = document.getElementById ('lawful-button');
+const lawfulImage = document.getElementById('lawful-image');
+//neutral
+const neutralInput = document.getElementById ('neutral-input');
+const neuButton = document.getElementById ('neutral-button');
+const neutralImage = document.getElementById('neutral-image');
+//chaotic
+const chaoticInput = document.getElementById ('chaotic-input');
+const chaButton = document.getElementById ('chaotic-button');
+const chaoticImage = document.getElementById('chaotic-image');
+
+
+//set event listeners to update state & DOM
+
+//lawful
+lawButton.addEventListener('click', () => {
+    const lawfulLevel = lawfulInput.value;
+    const lawfulUrl = getLawfulImage(lawfulLevel);
+    lawfulImage.src = lawfulUrl;
+});
+//neutral
+neuButton.addEventListener('click', () => {
+    const neutralLevel = neutralInput.value;
+    const neutralUrl = getNeutralImage(neutralLevel);
+    neutralImage.src = neutralUrl;
+});
+//chaotic
+chaButton.addEventListener('click', () => {
+    const chaoticLevel = chaoticInput.value;
+    const chaoticUrl = getChaoticImage(chaoticLevel);
+    chaoticImage.src = chaoticUrl;
+});
---
+++ utils.js
@@ -0,0 +1,30 @@
+
+export function getLawfulImage(someLawfulNumber) {
+    if (someLawfulNumber > 7) {
+        return './assets/lawfulgood.jpg';
+    } else if (someLawfulNumber > 4) {
+        return '/assets/lawfulneutral.jpg';
+    } else {
+        return '/assets/lawfulevil.jpg';
+    }
+}
+
+export function getNeutralImage(someNeutralNumber) {
+    if (someNeutralNumber > 7) {
+        return './assets/neutralgood.jpg';
+    } else if (someNeutralNumber > 4) {
+        return '/assets/trueneutral.jpg';
+    } else {
+        return '/assets/neutralevil.jpg';
+    }
+}
+
+export function getChaoticImage(someChaoticNumber) {
+    if (someChaoticNumber > true) {
+        return './assets/chaoticgood.jpg';
+    } else if (someChaoticNumber > 4) {
+        return '/assets/chaoticneutral.jpg';
+    } else {
+        return '/assets/chaoticevil.jpg';
+    }
+}
